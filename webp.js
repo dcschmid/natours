2752a737d6799ba1dfde16342bd8c3b6d94fd498
Daniel Schmid <daniel.schmid@libertymail.net>
2018-08-03 13:36:55 +0200

    Writing media queries - Base, Typography and Layout

--- webp.js
+++ webp.js
@@ -1,7 +1,7 @@
 const imagemin = require("imagemin");
 const webp = require("imagemin-webp");
 
-imagemin(["img/*.jpg"], "images", {
+imagemin(["img/*.jpg","img/*.png"], "images", {
   use: [
     webp({
       quality: 75
